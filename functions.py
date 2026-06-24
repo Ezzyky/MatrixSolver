ba5370993f9e3d5cf5f99f8a641fc11f40949c17
Ezzyky <abdellahezzyky@gmail.com>
2026-06-24 20:42:05 +0100

    add

---
+++ functions.py
@@ -0,0 +1,35 @@
+def prenant_matrice():
+    print("Saisissez la taille de la matrice:")
+    lignes=int(input("Nombre de lignes : "))
+    colonnes=int(input("Nombre de colonnes : "))
+    while lignes==0 or colonnes == 0:
+        print("Erreur : le nombre de lignes et de colonnes doit être supérieur à 0.")
+        lignes=int(input("Nombre de lignes : "))
+        colonnes=int(input("Nombre de colonnes : "))
+    Matrice=[[None for i in range(colonnes)]for i in range (lignes)]
+    for i in range(lignes):
+        for j in range (colonnes):
+            Matrice[i][j]=int(input(f"M[{i+1}][{j+1}] = "))
+    return Matrice
+
+def affichage(matrice):
+    lignes=len(matrice)
+    colonnes=len(matrice[0])
+    for i in range(lignes):
+        for j in range(colonnes):
+
+            print(matrice[i][j],end=" ")
+        print()
+
+def elimination_gauss(matrice, pivot):
+    lignes = len(matrice)
+    colonnes = len(matrice[0])
+
+    for j in range(pivot + 1, lignes):
+        if matrice[j][pivot] != 0:
+
+            a = matrice[j][pivot]
+            p = matrice[pivot][pivot]
+
+            for i in range(pivot, colonnes):
+                matrice[j][i] = matrice[j][i] * p - matrice[pivot][i] * a
---
+++ main.py
@@ -0,0 +1,46 @@
+from functions import *
+matrice=prenant_matrice()
+lignes=len(matrice)
+colonnes=len(matrice[0])
+print(affichage(matrice))
+# Permutation des lignes L1 et la première ligne dont le premier élément est non nul
+index=0
+if lignes>=2:
+    if matrice[0][0]==0:
+
+        for i in range(lignes):
+            if matrice[i][0]!=0:
+                index=i
+                break
+
+        for i in range(colonnes):
+            temp=matrice[0][i]
+            matrice[0][i]=matrice[index][i]
+            matrice[index][i]=temp
+        print(f"L1 ​↔ L​{index+1}")
+        affichage(matrice)
+for pivot in range(min(len(matrice), len(matrice[0]))):
+    elimination_gauss(matrice, pivot)
+affichage(matrice)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if matrice[0][0]!=0:
+#     for j in range(1,lignes):
+#         for i in range (colonnes):
+#             if matrice[j][0]!=0:
+#                 a= matrice[j][0]
+#                 for i in range(colonnes):
+#                     matrice[j][i] = (matrice[j][i] * matrice[0][0]- matrice[0][i] * a)
