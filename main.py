3b9670c7f3f410a726c4d178d06401f52ff8605a
Ezzyky <abdellahezzyky@gmail.com>
2026-06-25 15:12:33 +0100

    fix: refactor main.py for improved user interaction and error handling

--- main.py
+++ main.py
@@ -3,90 +3,108 @@ import msvcrt
 import os
 import sys
 os.system("cls") 
+#
 
-#affichage de menu et input d option et clear
-menu()
-option=int(input("Choisissez une option : "))
-os.system("cls") 
 while True:
-    #option 1 = reduction de gauss
-    if option==1:
-        print("""
-=========================================================
-                 Réduction de Gauss
-=========================================================
-            """)
-        #handling the Error of value
-        flag=0
-        try:
-            matrice=prenant_matrice()
-        except ValueError:
-            print("Veuillez entrer un nombre valide !")
-            print("Appuyez sur n'importe quel bouton pour sortir...")
-            msvcrt.getch()
-            os.system("cls")
-            flag=1
-        if flag==0:
-            print()
-            lignes=len(matrice)
-            colonnes=len(matrice[0])
-            os.system("cls")
-            print("""
-=========================================================
-                 Réduction de Gauss
-=========================================================
-            """)
-            print("Matrice initiale :")
-            affichage(matrice)
-            # Permutation des lignes L1 et la première ligne dont le premier élément est non nul
-            index=0
-            if lignes>=2:
-                if matrice[0][0]==0:
 
-                    for i in range(lignes):
-                        if matrice[i][0]!=0:
-                            index=i
-                            break
+    print("""
+            =========================================================
+                BIENVENUE DANS LE PROGRAMME D'ALGÈBRE LINÉAIRE
+            =========================================================
 
-                    for i in range(colonnes):
-                        temp=matrice[0][i]
-                        matrice[0][i]=matrice[index][i]
-                        matrice[index][i]=temp
-                    print(f"L1 ​↔ L​{index+1}")
-                    affichage(matrice)
-                for pivot in range(0,min(len(matrice), len(matrice[0]))):
-                    if matrice[pivot][pivot] == 0:
-                        index = -1
-                        for i in range(pivot + 1, lignes):
-                            if matrice[i][pivot] != 0:
-                                index = i
-                                break
-                        if index != -1:
-                            matrice[pivot], matrice[index] = matrice[index], matrice[pivot]
-                            print(f"L{pivot+1} ↔ L{index+1}")
-                    elimination_gauss(matrice, pivot)
-                    affichage(matrice)
-                    print()
-            else:
-                pass
-            #affichge de resulta fianle
-            print("resulta finale:")
-            affichage(matrice)
-            print()
-            print("Appuyez sur n'importe quel bouton pour sortir...")
-            msvcrt.getch()
-            os.system("cls") 
+            Ce programme permet d'effectuer plusieurs opérations sur
+            les matrices et les systèmes linéaires.
 
-    #affichage de menu et input d option et clear
-    menu() 
-    option=int(input("Choisissez une option : "))
+            Veuillez d'abord saisir la matrice initiale, puis choisir
+            l'opération souhaitée dans le menu suivant.
+
+    """)
+    print("Appuyez sur n'importe quel bouton pour contune...")
+    msvcrt.getch()
     os.system("cls")
-    #option 2 = Méthode de Gauss-Jordan
-    if option==2:
-        pass
-    #option 5 = Addition de matrices
-    if option==5:
-        pass
-    if option==0:
-        print("GOOD BYE!")
-        sys.exit()
+    print("""
+            =========================================================
+                    Veuillez entrer la matrice initiale.
+            =========================================================
+    """)
+    flag=0
+    try:
+        matrice=prenant_matrice()
+    except ValueError:
+                print("Veuillez entrer un nombre valide !")
+                print("Appuyez sur n'importe quel bouton pour sortir...")
+                msvcrt.getch()
+                os.system("cls")
+                flag=1
+                continue
+    if flag!=0:
+        #affichage de menu et input d option et clear
+        os.system("cls")
+        menu()
+        option=int(input("Choisissez une option : "))
+        os.system("cls") 
+
+
+        #option 1 = reduction de gauss
+        if option==1:
+                lignes=len(matrice)
+                colonnes=len(matrice[0])
+                print("""
+    =========================================================
+                    Réduction de Gauss
+    =========================================================
+                """)
+                print("Matrice initiale :")
+                affichage(matrice)
+                # Permutation des lignes L1 et la première ligne dont le premier élément est non nul
+                index=0
+                if lignes>=2:
+                    if matrice[0][0]==0:
+
+                        for i in range(lignes):
+                            if matrice[i][0]!=0:
+                                index=i
+                                break
+
+                        for i in range(colonnes):
+                            temp=matrice[0][i]
+                            matrice[0][i]=matrice[index][i]
+                            matrice[index][i]=temp
+                        print(f"L1 ​↔ L​{index+1}")
+                        affichage(matrice)
+                    for pivot in range(0,min(len(matrice), len(matrice[0]))):
+                        if matrice[pivot][pivot] == 0:
+                            index = -1
+                            for i in range(pivot + 1, lignes):
+                                if matrice[i][pivot] != 0:
+                                    index = i
+                                    break
+                            if index != -1:
+                                matrice[pivot], matrice[index] = matrice[index], matrice[pivot]
+                                print(f"L{pivot+1} ↔ L{index+1}")
+                        elimination_gauss(matrice, pivot)
+                        affichage(matrice)
+                        print()
+                else:
+                    pass
+                #affichge de resulta fianle
+                print("resulta finale:")
+                affichage(matrice)
+                print()
+                print("Appuyez sur n'importe quel bouton pour sortir...")
+                msvcrt.getch()
+                os.system("cls")
+
+        #affichage de menu et input d option et clear
+        menu() 
+        option=int(input("Choisissez une option : "))
+        os.system("cls")
+        #option 2 = Méthode de Gauss-Jordan
+        if option==2:
+            pass
+        #option 5 = Addition de matrices
+        if option==5:
+            pass
+        if option==0:
+            print("GOOD BYE!")
+            sys.exit()
